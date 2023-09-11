14654bcccb44a6faa15c58ae0a14d058a916854b
Agustin <agustin@ini.live>
2023-09-11 08:45:10 -0300

    ADD main

--- src/main/java/ar/edu/undef/fie/Main.java
+++ src/main/java/ar/edu/undef/fie/Main.java
@@ -1,19 +1,11 @@
 package ar.edu.undef.fie;
 
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
-
-            // Press Shift+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
-        }
+        var suma = 0;
+        for (var arg:args)
+            suma += Integer.parseInt(arg);
+        System.out.println("El promedio de los numeros es: " + suma/ args.length);
     }
 }
