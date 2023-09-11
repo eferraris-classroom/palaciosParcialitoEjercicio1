e7b588f125bbd47d28fee555e5c88d17aa1af7f1
Agustin <agustin@ini.live>
2023-09-11 09:08:28 -0300

    FIX main

--- src/main/java/ar/edu/undef/fie/Main.java
+++ src/main/java/ar/edu/undef/fie/Main.java
@@ -1,11 +1,13 @@
 package ar.edu.undef.fie;
 
+//Hacer un programa que calcule el promedio de los números pasados por argumento a través de la consola,
+// y lo muestre por la pantalla.
 public class Main {
     public static void main(String[] args) {
 
-        var suma = 0;
+        float suma = 0; // lo declaro como float para que el promedio sea exacto
         for (var arg:args)
             suma += Integer.parseInt(arg);
-        System.out.println("El promedio de los numeros es: " + suma/ args.length);
+        System.out.println("El promedio de los numeros es: " + suma/args.length);
     }
 }
